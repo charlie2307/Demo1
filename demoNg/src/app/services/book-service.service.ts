3ad133274a279eddee77b4d290bdef88339ac7cf
charlie2307 <riteshalim237@gmail.com>
2025-05-23 10:37:06 +0530

    bookservice

--- demoNg/src/app/services/book-service.service.ts
+++ demoNg/src/app/services/book-service.service.ts
@@ -7,12 +7,19 @@ import { Observable } from 'rxjs';
 })
 export class BookServiceService {
 
-  private apiUrl = 'https://localhost:7252/api/IssueBook/ViewAllIssueBook';
+  private apiUrl = 'https://localhost:7252/api/IssueBook/';
 
   constructor(private http:HttpClient) { }
 
   getAllBooks(): Observable<any>{
     return this.http.get(this.apiUrl);
+  }
+
+    // Email  
+  sendEmail() {
+    return this.http.get(this.apiUrl + 'SendEmailForPendingReturns', {
+      responseType: 'text',
+    });
   }
 
 }
